5b53d772d6c095aeeb5c9d478d7955d7cbfee02b
Bassim Durzi <bassimdurzi@gmail.com>
2023-10-18 09:31:27 +0200

    Part 1

---
+++ src/main/java/example/libraryjpaapp/LibraryJpaAppApplication.java
@@ -0,0 +1,13 @@
+package example.libraryjpaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryJpaAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryJpaAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/example/libraryjpaapp/LibraryJpaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package example.libraryjpaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryJpaAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
